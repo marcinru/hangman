87bd33813b5e553f0b51db87d9ae4c0e91e133ec
Marcin Rudzki <mar.rudzki@gmail.com>
2023-06-11 13:51:38 +0200

    remove not used assets

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const hiddenWord = 'apple'.split('');
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <h1>Hangman</h1>
+      <h2>Lives 3/5</h2>
+      <div className="guessed">
+        A____
       </div>
-      <h1>Vite + React</h1>
+      <input id="letter" type="text" placeholder="Enter a letter" />
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button onClick={() => {
+          const input = document.getElementById('letter') as HTMLInputElement;
+          if (hiddenWord.includes(input.value.toLowerCase())) {
+            console.log('guessed');
+          } else {
+            console.log('not guessed');
+          }
+          input.value = '';
+        }}>
+          Confirm
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
