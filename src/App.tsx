e566214a0c80aded7fbe605336b88445e7925466
Marcin Rudzki <mar.rudzki@gmail.com>
2023-06-11 18:04:04 +0200

    add setGuessed

--- src/App.tsx
+++ src/App.tsx
@@ -2,24 +2,29 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [lives, setLives] = useState(5);
   const hiddenWord = 'apple'.split('');
+  const [lives, setLives] = useState(5);
+  const [guessed, setGuessed] = useState(new Array(hiddenWord.length).fill('_').join(''));
 
   return (
     <>
       <h1>Hangman</h1>
       <h2>Lives {lives}/5</h2>
-      <div className="guessed">
-        A____
-      </div>
+      <div className="guessed">{guessed}</div>
       <input id="letter" type="text" placeholder="Enter a letter" />
       <div className="card">
         <button onClick={() => {
           const input = document.getElementById('letter') as HTMLInputElement;
-          if (hiddenWord.includes(input.value.toLowerCase())) {
-            console.log('guessed');
+          const letter = input.value.toLowerCase();
+          if (hiddenWord.includes(letter)) {
+            const guessedArray = guessed.split('');
+            hiddenWord.forEach((char, index) => {
+              if (char === letter) {
+                guessedArray[index] = letter;
+              }
+              setGuessed(guessedArray.join(''));
+            })
           } else {
-            console.log('not guessed, you lose 1 life');
             setLives((lives) => lives - 1)
           }
           input.value = '';
