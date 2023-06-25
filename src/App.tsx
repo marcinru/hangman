0a68338837cdb2dd794a18661ce291bda1cb7dff
Marcin Rudzki <mar.rudzki@gmail.com>
2023-06-25 06:07:06 +0200

    extract the guess function

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,23 @@ function App() {
   const [lives, setLives] = useState(5);
   const [guessed, setGuessed] = useState(new Array(hiddenWord.length).fill('_').join(''));
 
+  const guess = () => {
+    const input = document.getElementById('letter') as HTMLInputElement;
+    const letter = input.value.toLowerCase();
+    if (hiddenWord.includes(letter)) {
+      const guessedArray = guessed.split('');
+      hiddenWord.forEach((char, index) => {
+        if (char === letter) {
+          guessedArray[index] = letter;
+        }
+        setGuessed(guessedArray.join(''));
+      })
+    } else {
+      setLives((lives) => lives - 1)
+    }
+    input.value = '';
+  }
+
   return (
     <>
       <h1>Hangman</h1>
@@ -13,22 +30,7 @@ function App() {
       <div className="guessed">{guessed}</div>
       <input id="letter" type="text" placeholder="Enter a letter" />
       <div className="card">
-        <button onClick={() => {
-          const input = document.getElementById('letter') as HTMLInputElement;
-          const letter = input.value.toLowerCase();
-          if (hiddenWord.includes(letter)) {
-            const guessedArray = guessed.split('');
-            hiddenWord.forEach((char, index) => {
-              if (char === letter) {
-                guessedArray[index] = letter;
-              }
-              setGuessed(guessedArray.join(''));
-            })
-          } else {
-            setLives((lives) => lives - 1)
-          }
-          input.value = '';
-        }}>
+        <button onClick={guess}>
           Confirm
         </button>
       </div>
