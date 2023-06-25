cb1cf048d233c4f30ed06191910c408bad2f9a0c
Marcin Rudzki <mar.rudzki@gmail.com>
2023-06-25 07:32:18 +0200

    handle onLetterChange via useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,30 @@
-import { useState } from 'react'
+import { useState, FormEvent, ChangeEvent } from 'react'
 import './App.css'
 
 function App() {
-  const hiddenWord = 'apple'.split('');
-  const [lives, setLives] = useState(5);
-  const [guessed, setGuessed] = useState(new Array(hiddenWord.length).fill('_').join(''));
+  const hiddenWord = 'apple'.split('')
+  const [letter, setLetter] = useState('')
+  const [lives, setLives] = useState(5)
+  const [guessed, setGuessed] = useState(new Array(hiddenWord.length).fill('_').join(''))
 
-  const guess = () => {
-    const input = document.getElementById('letter') as HTMLInputElement;
-    const letter = input.value.toLowerCase();
+  const guess = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
     if (hiddenWord.includes(letter)) {
-      const guessedArray = guessed.split('');
+      const guessedArray = guessed.split('')
       hiddenWord.forEach((char, index) => {
         if (char === letter) {
-          guessedArray[index] = letter;
+          guessedArray[index] = letter
         }
-        setGuessed(guessedArray.join(''));
+        setGuessed(guessedArray.join(''))
       })
     } else {
       setLives((lives) => lives - 1)
     }
-    input.value = '';
+    setLetter('')
+  }
+
+  const onLetterChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setLetter(event.target.value)
   }
 
   return (
@@ -28,12 +32,12 @@ function App() {
       <h1>Hangman</h1>
       <h2>Lives {lives}/5</h2>
       <div className="guessed">{guessed}</div>
-      <input id="letter" type="text" placeholder="Enter a letter" />
-      <div className="card">
-        <button onClick={guess}>
+      <form onSubmit={guess}>
+        <input value={letter} onChange={onLetterChange} placeholder="Enter a letter" autoComplete="off" />
+        <button type="submit">
           Confirm
         </button>
-      </div>
+      </form>
     </>
   )
 }
