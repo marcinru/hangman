b0c2f12056c8d29b9746a6f974bec4655782b509
Marcin Rudzki <mar.rudzki@gmail.com>
2023-07-01 19:14:04 +0200

    Add container and autoFocus

--- src/App.tsx
+++ src/App.tsx
@@ -28,19 +28,26 @@ function App() {
   }
 
   return (
-    <>
+    <div className="container">
       <h1>Hangman</h1>
       <h2>Lives {lives}/5</h2>
       <div className="guessed">{guessed}</div>
       <form className={styles.form} onSubmit={guess}>
         <div className="input-group mb-3">
-          <input className="form-control" value={letter} onChange={onLetterChange} placeholder="Enter a letter" autoComplete="off" />
+          <input
+              autoComplete="off"
+              autoFocus
+              className="form-control"
+              onChange={onLetterChange}
+              placeholder="Enter a letter"
+              value={letter}
+          />
           <button type="submit" className="btn btn-outline-secondary">
             Confirm
           </button>
         </div>
       </form>
-    </>
+    </div>
   )
 }
 
