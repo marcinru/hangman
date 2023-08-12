89e568f6cef18dd44d502782ba8efcf11359dabb
Marcin Rudzki <mar.rudzki@gmail.com>
2023-08-12 10:04:07 +0200

    styling the form

--- src/App.tsx
+++ src/App.tsx
@@ -43,7 +43,8 @@ function App() {
       <header className="border-b border-neutral-500 py-16 mb-16">
         <h1 className="text-5xl">Hangman</h1>
       </header>
-      <div className="bg-white rounded-lg px-6 py-3 flex divide-x divide-neutral-500 w-fit">
+
+      <div className="bg-white rounded-lg px-6 py-3 flex divide-x divide-neutral-500 w-fit mb-12">
         <div className="w-36">
           <h2 className="text-2xl">Lives</h2>
           <output>{lives}/5</output>
@@ -53,34 +54,38 @@ function App() {
           <output>fruits</output>
         </div>
       </div>
-      <div className="font-mono tracking-widest">{guessed}</div>
-      <form className="" onSubmit={guess}>
-        <input
-            autoComplete="off"
-            autoFocus
-            className="my-2 rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
-            onChange={onLetterChange}
-            placeholder="Enter a letter"
-            type="text"
-            value={letter}
-        />
-        <button
-            className="uppercase inline-flex items-center justify-center px-4 py-2 text-base font-medium leading-6 text-white whitespace-no-wrap bg-blue-600 border border-blue-700 rounded-md shadow-sm hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
-            data-rounded="rounded-md"
-            data-primary="blue-600"
-            data-primary-reset="{}"
-            type="submit"
-        >
-          Confirm
-        </button>
-      </form>
-      {win && <Alert type="success" message="Congrats! You've guessed it!" />}
-      {lost && <Alert type="danger" message="Game over!" />}
-      {(win || lost) && (
-        <button className="btn btn-primary btn-lg" onClick={startNewGame}>
-          New game?
-        </button>
-      )}
+
+      <div className="bg-white rounded-lg px-6 py-3 w-fit">
+        <div className="font-mono tracking-widest">{guessed}</div>
+        <form className="my-2 flex items-center" onSubmit={guess}>
+          <input
+              autoComplete="off"
+              autoFocus
+              className="mr-9 rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
+              onChange={onLetterChange}
+              placeholder="Enter a letter"
+              type="text"
+              value={letter}
+          />
+          <button
+              className="uppercase text-white bg-orange-700 border border-orange-700 rounded-md px-5 py-2"
+              type="submit"
+          >
+            Confirm
+          </button>
+        </form>
+        {win && <Alert type="success" message="Congrats! You've guessed it!" />}
+        {lost && <Alert type="danger" message="Game over!" />}
+        {(win || lost) && (
+          <button
+              className="uppercase text-white bg-orange-700 border border-orange-700 rounded-md px-5 py-2"
+              onClick={startNewGame}
+              type="button"
+          >
+            New game?
+          </button>
+        )}
+      </div>
     </div>
   );
 }
